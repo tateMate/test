29ee1c6004ce02acc59df849b3a306c63c8ad14d
zzinimon <jeeheekim94@naver.com>
2021-12-23 19:14:32 +0900

    Show and Insert Cocomment(complete), Delete Comment And Cocomment(ing)

--- demo/src/main/java/edu/test/demo/controller/TestController.java
+++ demo/src/main/java/edu/test/demo/controller/TestController.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,30 +15,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.test.demo.service.CocommentService;
+import edu.test.demo.service.CommentService;
 import edu.test.demo.service.UserService;
+import edu.test.demo.vo.CocommentVO;
+import edu.test.demo.vo.CommentVO;
+import edu.test.demo.vo.NicknamedCocommentVO;
+import edu.test.demo.vo.NicknamedCommentVO;
 import edu.test.demo.vo.UserVO;
 
 @RestController
 public class TestController {
 	@Autowired
 	UserService userService;
+	@Autowired
+	CommentService commentService;
+	@Autowired
+	CocommentService cocommentService;
 	
 	
-	@GetMapping("/user")
+	@GetMapping("/rest/user")
 	public List<UserVO> selectUser(@RequestParam(required = false) Integer id) {
 		List<UserVO> li=new ArrayList<UserVO>();
 		li.add(userService.selectUserByUserId(id));
 		if(id==null) return userService.selectUser();
 		return li;
 	}
-	@GetMapping("/test")
-	public String test() {
-		return "�쁽�옱 �떆媛곸� "+new Date() + "�엯�땲�떎.";
+	@GetMapping("/rest/booleantest")
+	public boolean booleanTest(@RequestParam(required = false) Integer id) {
+		boolean chk = true;
+		boolean chk2 = false;
+		boolean rst;
+		if(id!=null) {
+			rst = (id==1)?chk:chk2;
+		}else rst = false;
+		return rst;
 	}
-	@PostMapping("/api/users")
-	public UserVO users() {
+	@GetMapping("/rest/test")
+	public List test() {
+		int user_id = 2;
+		JSONArray ja = cocommentService.selectCocommentByCommentIdTo(user_id);
+		return ja;
+	}
+	@GetMapping("/api/users")
+	public List<UserVO> users() {
 		List<UserVO> userList = userService.selectUser();
-		UserVO user = userList.get(0);
-		return user;
+		return userList;
 	}
 }
